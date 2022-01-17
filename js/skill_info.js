5b8925f88edacf00e2158ded8a11900f0db497c2
bruceyu0925 <bruceyu0925@gmail.com>
2022-01-17 21:48:14 +0800

    restful 架構

--- js/skill_info.js
+++ js/skill_info.js
@@ -6,81 +6,183 @@ const FormId         = getId( 'FormId' ),
       FormScore      = getId( 'FormScore' ),
       FormDateBuild  = getId( 'FormDateBuild' ),
       FormDateUpdate = getId( 'FormDateUpdate' ),
-      FormBtnLs      = getId( 'FormBtnLs' ),
-      FormCancel     = getId( 'FormCancel' ),
-      FormApply      = getId( 'FormApply' );
+      FormBtnLs      = getId( 'FormBtnLs' );
 
-// carry out 解析網址參數、fetch
-if( location.href.indexOf( '?' ) !== -1 ) {
+var ReqId;
+var Ary_List = [],
+    Ary_Kind = [];
 
-    var k = location.href.split( '?' )[ 1 ],
-        i = Number( k.replace( 'id=' , '' ) );
+// 判斷 舊資料/新資料
+try {
+
+    ReqId = ( location.href.split( '?' )[ 1 ] ).replace( 'id=' , '' );
+
+    fetch( GAS( 'AKfycbxlvF-Cw1I2YXHe6_9w3RPcbA57tne9GPGgUOIAOkoHjbMB4TGGEm8On_-p3C-WNaud' ) , {
+        method: 'POST',
+        headers: { 'Content-Type' : 'application/x-www-form-urlencoded; charset=utf-8' },
+        body: JSON.stringify({
+            type : 'GET',
+            id   : ReqId
+        })
+    
+    }).then( ( res ) => {
+        return res.json()
+    
+    }).then( ( data ) => {
+        Ary_List = data;
+        FormHtml();
+    })
+
+} catch {
+
+    FormBtnLs.insertAdjacentHTML( 'beforeend' ,
+        `<div class="form-group">
+            <button class="form-btn" id="FormCancel" onclick="FormCancel()">取消</button>
+        </div>
+        <div class="form-group">
+            <button class="form-btn" id="FormBuild" onclick="FormBuild()">建立</button>
+        </div>`
+    )
+
+    Loading( false );
+
+};
+
+// func 輸出資料
+FormHtml = () => {
+
+    setTimeout( () => {
+
+        // 設定資料
+        FormTitle     .value = Ary_List[ 'Title' ];
+        FormDesc      .value = Ary_List[ 'Desc' ];
+        FormKind      .value = Ary_List[ 'Kind' ];
+        FormScore     .value = Ary_List[ 'Score' ];
+        FormId        .value = Ary_List[ 'Id' ];
+        FormDateBuild .value = DateTran( Ary_List[ 'DateBuild' ] );
+        FormDateUpdate.value = DateTran( Ary_List[ 'DateUpdate' ] );
+
+        // 刪除按鈕
+        FormBtnLs.insertAdjacentHTML( 'beforeend' ,
+            `<div class="form-group">
+                <button class="form-btn" id="FormDelete" onclick="FormDelete()">刪除</button>
+            </div>
+            <div class="form-group">
+                <button class="form-btn" id="FormCancel" onclick="FormCancel()">取消</button>
+            </div>
+            <div class="form-group">
+                <button class="form-btn" id="FormApply" onclick="FormApply()">確定</button>
+            </div>`
+        )
+
+        Loading( false );
+
+    } , 500 )
+}
+
+// func 刪除
+FormDelete = () => {
+
+    if ( confirm( '確定刪除此項設定嗎？' ) ) {
+
+        Loading( true );
+
+        fetch( GAS( 'AKfycbxlvF-Cw1I2YXHe6_9w3RPcbA57tne9GPGgUOIAOkoHjbMB4TGGEm8On_-p3C-WNaud' ) , {
+            method: 'POST',
+            headers: { 'Content-Type' : 'application/x-www-form-urlencoded; charset=utf-8' },
+            body: JSON.stringify({
+                type : 'DELETE',
+                id   : ReqId
+            })
+        
+        }).then( ( res ) => {
+            return res.text()
+        
+        }).then( ( data ) => {
+    
+            window.location.href = '/backstage/skill_list.html';
+    
+        })
+    }
+}
+
+// func 取消
+FormCancel = () => {
+
+    if ( confirm( '確認離開？將不保存此次設定。' ) ) {
+
+        window.location.href = '/backstage/skill_list.html';
+
+    }
+}
+
+// func 修改
+FormApply = () => {
 
     Loading( true );
 
     fetch( GAS( 'AKfycbxlvF-Cw1I2YXHe6_9w3RPcbA57tne9GPGgUOIAOkoHjbMB4TGGEm8On_-p3C-WNaud' ) , {
         method: 'POST',
         headers: { 'Content-Type' : 'application/x-www-form-urlencoded; charset=utf-8' },
-        body: JSON.stringify({ 'id' : i })
-
+        body: JSON.stringify({
+            type  : 'PUT',
+            id    : ReqId,
+            title : FormTitle.value,
+            desc  : FormDesc .value,
+            kind  : FormKind .value,
+            score : FormScore.value
+        })
+    
     }).then( ( res ) => {
-        return res.json()
-
+        return res.text()
+    
     }).then( ( data ) => {
 
-        var d = data[ 0 ];
+        window.location.href = '/backstage/skill_list.html';
 
-        setTimeout( () => {
-
-            // 設定資料
-            FormTitle     .value = d[ 'Title' ];
-            FormDesc      .value = d[ 'Desc' ];
-            FormKind      .value = d[ 'Kind' ];
-            FormScore     .value = d[ 'Score' ];
-            FormId        .value = d[ 'Id' ];
-            FormDateBuild .value = DateTran( d[ 'DateBuild' ] );
-            FormDateUpdate.value = DateTran( d[ 'DateUpdate' ] );
-
-            // 刪除按鈕
-            FormBtnLs.insertAdjacentHTML( 'afterbegin' ,
-                `<div class="form-group">
-                    <button class="form-btn" id="FormDelete">刪除</button>
-                </div>`
-            )
-            getId( 'FormDelete' ).onclick = () => {
-                window.location.href = '/backstage/skill_list.html';
-            }
-
-            Loading( false );
-
-        } , 500 )
     })
 }
+
+// func 建立
+FormBuild = () => {
+
+    Loading( true );
+
+    fetch( GAS( 'AKfycbxlvF-Cw1I2YXHe6_9w3RPcbA57tne9GPGgUOIAOkoHjbMB4TGGEm8On_-p3C-WNaud' ) , {
+        method: 'POST',
+        headers: { 'Content-Type' : 'application/x-www-form-urlencoded; charset=utf-8' },
+        body: JSON.stringify({
+            type  : 'POST',
+            title : FormTitle.value,
+            desc  : FormDesc .value,
+            kind  : FormKind .value,
+            score : FormScore.value
+        })
+    
+    }).then( ( res ) => {
+        return res.text()
+    
+    }).then( ( data ) => {
+
+        window.location.href = '/backstage/skill_list.html';
+
+    })
+}
+
+// event 若有修改，離開頁面前詢問
+// queAll( '.form-input' ).forEach( el => {
+
+//     el.onchange = () => {
+
+//         window.onbeforeunload = ( e ) => {
+
+//             var e = window.event || e;
+//             e.returnValue = true;
+//         }
+//     }
+// });
 
 // event 轉換整數
 FormScore.onkeyup = function() {
     this.value = Math.round( this.value );
-}
-
-// event 取消設定
-FormCancel.onclick = () => {
-    window.location.href = '/backstage/skill_list.html';
-}
-
-// event 若有修改，離開頁面前詢問
-queAll( '.form-input' ).forEach( el => {
-
-    el.onchange = () => {
-
-        window.onbeforeunload = ( e ) => {
-
-            var e = window.event || e;
-            e.returnValue = true;
-        }
-    }
-});
-
-// event 確認設定
-FormApply.onclick = () => {
-    window.location.href = '/backstage/skill_list.html';
 }
