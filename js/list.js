c2d4e1e6866cbb39be0e1c79219386bd1bf7ea6a
bruceyu0925 <bruceyu0925@gmail.com>
2022-02-03 19:39:52 +0800

    ts

--- js/list.js
+++ js/list.js
@@ -1,182 +1,181 @@
-var List_Array = [],
-    List_Total,
-    Search_Array = [],
-    Search_Total,
-    Page_All,
-    Page_Num,
-    Page_Len = 10;
+// var List_Array = [],
+//     List_Total,
+//     Search_Array = [],
+//     Search_Total,
+//     Page_All,
+//     Page_Num,
+//     Page_Len = 10;
 
-// Page --------------------------------------------------
+// // Page --------------------------------------------------
 
-// func 產生頁碼
-const PageHtml = () => {
+// // func 產生頁碼
+// const PageHtml = () => {
 
-    getId( 'PageList' ).innerHTML = '';
+//     getId( 'PageList' ).innerHTML = '';
 
-    var l = [];
+//     var l = [];
     
-    // 頁碼dom
-    const p = ( n ) => {
+//     // 頁碼dom
+//     const p = ( n ) => {
 
-        var h = `<button class="page-li page-btn" id="${ 'Page' + n }" onclick="PageClick(${ n })">${ n }</button>`;
+//         var h = `<button class="page-li page-btn" id="${ 'Page' + n }" onclick="PageClick(${ n })">${ n }</button>`;
 
-        if( n === '' ) h = '<div class="page-dot">···<div>';
+//         if( n === '' ) h = '<div class="page-dot">···<div>';
 
-        return `<li>${ h }</li>`;
-    };
+//         return `<li>${ h }</li>`;
+//     };
 
-    // 不超過7頁
-    if ( Page_All <= 7 ) {
+//     // 不超過7頁
+//     if ( Page_All <= 7 ) {
 
-        for( let i = 1 ; i <= Page_All ; i++ ) {
-            l.push( i )
-        }
+//         for( let i = 1 ; i <= Page_All ; i++ ) {
+//             l.push( i )
+//         }
 
-    // 超過7頁
-    } else {
+//     // 超過7頁
+//     } else {
 
-        var f = Page_Num - 2,
-            b = Page_Num + 2;
+//         var f = Page_Num - 2,
+//             b = Page_Num + 2;
 
-        // 第一頁
-        if( f - 1 === 1 ) {
-            l.push( 1 )
+//         // 第一頁
+//         if( f - 1 === 1 ) {
+//             l.push( 1 )
 
-        } else if( f - 1 > 1 ) {
-            l.push( 1 , '' )
-        }
+//         } else if( f - 1 > 1 ) {
+//             l.push( 1 , '' )
+//         }
 
-        // 前後2頁
-        if( f - 1 < 1 ) {
-            f = 1;
-            b = f + 4;
+//         // 前後2頁
+//         if( f - 1 < 1 ) {
+//             f = 1;
+//             b = f + 4;
 
-        } else if( b + 1 > Page_All ) {
-            b = Page_All;
-            f = b - 4;
-        }
+//         } else if( b + 1 > Page_All ) {
+//             b = Page_All;
+//             f = b - 4;
+//         }
 
-        for( let i = f ; i <= b ; i++ ){
-            l.push( i )
-        }
+//         for( let i = f ; i <= b ; i++ ){
+//             l.push( i )
+//         }
         
-        // 最終頁
-        if( b + 1 === Page_All ) {
-            l.push( Page_All )
+//         // 最終頁
+//         if( b + 1 === Page_All ) {
+//             l.push( Page_All )
 
-        } else if( b + 1 < Page_All ) {
-            l.push( '' , Page_All )
-        }
-    }
-    // 產生dom
-    l.forEach( el => 
-        getId( 'PageList' ).insertAdjacentHTML( 'beforeend' , p( el ) ) 
-    );
-};
+//         } else if( b + 1 < Page_All ) {
+//             l.push( '' , Page_All )
+//         }
+//     }
+//     // 產生dom
+//     l.forEach( el => 
+//         getId( 'PageList' ).insertAdjacentHTML( 'beforeend' , p( el ) ) 
+//     );
+// };
 
-// func 頁碼點選
-const PageClick = ( n ) => {
-    Page_Num = n;
-    PageHtml();
-    ListHtml();
-};
+// // func 頁碼點選
+// const PageClick = ( n ) => {
+//     Page_Num = n;
+//     PageHtml();
+//     ListHtml();
+// };
 
-// func 頁碼判斷
-const PageJudge = () => {
+// // func 頁碼判斷
+// const PageJudge = () => {
 
-    if ( Page_All <= 1 ) {
-        getId( 'PagePrev' ).disabled = true;
-        getId( 'PageNext' ).disabled = true;
+//     if ( Page_All <= 1 ) {
+//         getId( 'PagePrev' ).disabled = true;
+//         getId( 'PageNext' ).disabled = true;
 
-    } else if ( Page_Num === 1 ) {
-        getId( 'PagePrev' ).disabled = true;
-        getId( 'PageNext' ).disabled = false;
+//     } else if ( Page_Num === 1 ) {
+//         getId( 'PagePrev' ).disabled = true;
+//         getId( 'PageNext' ).disabled = false;
 
-    } else if ( Page_Num === Page_All ) {
-        getId( 'PagePrev' ).disabled = false;
-        getId( 'PageNext' ).disabled = true;
+//     } else if ( Page_Num === Page_All ) {
+//         getId( 'PagePrev' ).disabled = false;
+//         getId( 'PageNext' ).disabled = true;
         
-    } else {
-        getId( 'PagePrev' ).disabled = false;
-        getId( 'PageNext' ).disabled = false;
-    };
+//     } else {
+//         getId( 'PagePrev' ).disabled = false;
+//         getId( 'PageNext' ).disabled = false;
+//     };
 
-    try {
-        queOne( '.page-li.--click' ).classList.remove( '--click' );
-    } catch {};
+//     try {
+//         queOne( '.page-li.--click' ).classList.remove( '--click' );
+//     } catch {};
 
-    try {
-        getId( 'Page' + Page_Num ).classList.add( '--click' );
-    } catch {};
-};
+//     try {
+//         getId( 'Page' + Page_Num ).classList.add( '--click' );
+//     } catch {};
+// };
 
-// event 上一頁
-getId( 'PagePrev' ).onclick = () => PageClick( Page_Num - 1 );
+// // event 上一頁
+// getId( 'PagePrev' ).onclick = () => PageClick( Page_Num - 1 );
 
-// event 下一頁
-getId( 'PageNext' ).onclick = () => PageClick( Page_Num + 1 );
+// // event 下一頁
+// getId( 'PageNext' ).onclick = () => PageClick( Page_Num + 1 );
 
 
-// List --------------------------------------------------
+// // List --------------------------------------------------
 
-// event 執行搜尋
-getId( 'FormSearch' ).onclick = () => {
+// // event 執行搜尋
+// getId( 'FormSearch' ).onclick = () => {
 
-    Loading( true );
-    ListFilter();
+//     Loading( true );
+//     ListFilter();
 
-    var s = queOne( '.td-sort-btn.--click' ),
-        v = s.value;
+//     var s = queOne( '.td-sort-btn.--click' ),
+//         v = s.value;
 
-    // // 處理排序
-    // Search_Array.sort( ( a , b ) => {
+//     // 處理排序
+//     Search_Array.sort( ( a , b ) => {
 
-    //     if( s.classList.contains( '--max' ) ) {
-    //         return a[ v ] < b[ v ] ? 1 : -1
+//         if( s.classList.contains( '--max' ) ) {
+//             return a[ v ] < b[ v ] ? 1 : -1
 
-    //     } else if( s.classList.contains( '--min' ) ) {
-    //         return a[ v ] > b[ v ] ? 1 : -1
-    //     }
-    // });
+//         } else if( s.classList.contains( '--min' ) ) {
+//             return a[ v ] > b[ v ] ? 1 : -1
+//         }
+//     });
     
-    // 處理參數
-    Search_Total = Search_Array.length;
-    alert( Search_Total );
-    Page_All     = Math.ceil( Search_Total / Page_Len );
-    Page_Num     = 1;
+//     // 處理參數
+//     Search_Total = Search_Array.length;
+//     Page_All     = Math.ceil( Search_Total / Page_Len );
+//     Page_Num     = 1;
     
-    setTimeout( () => {
+//     setTimeout( () => {
 
-        Loading( false );
-        PageHtml();
-        ListHtml();
+//         Loading( false );
+//         PageHtml();
+//         ListHtml();
 
-        getId( 'SearchTotal' ).innerHTML = '共 ' + Search_Total + ' 筆資料';
+//         getId( 'SearchTotal' ).innerHTML = '共 ' + Search_Total + ' 筆資料';
         
-    } , 300 );
-};
+//     } , 300 );
+// };
 
-// event 重設搜尋
-getId( 'FormReset' ).onclick = () => {
+// // event 重設搜尋
+// getId( 'FormReset' ).onclick = () => {
 
-    queAll( '.form-input' ).forEach( el => el.value = '' );
-    getId( 'FormSearch' ).click();
-};
+//     queAll( '.form-input' ).forEach( el => el.value = '' );
+//     getId( 'FormSearch' ).click();
+// };
 
-// event 切換排序
-queAll( '.td-sort-btn' ).forEach( el => {
+// // event 切換排序
+// queAll( '.td-sort-btn' ).forEach( el => {
 
-    el.onclick = () => {
+//     el.onclick = () => {
 
-        if( el.classList.contains( '--click' ) ) {
+//         if( el.classList.contains( '--click' ) ) {
 
-            el.classList.toggle( '--max' );
-            el.classList.toggle( '--min' );
-        }
+//             el.classList.toggle( '--max' );
+//             el.classList.toggle( '--min' );
+//         }
 
-        queOne( '.td-sort-btn.--click' ).classList.remove( '--click' );
-        el.classList.add( '--click' );
+//         queOne( '.td-sort-btn.--click' ).classList.remove( '--click' );
+//         el.classList.add( '--click' );
 
-        getId( 'FormSearch' ).click();
-    }
-});
+//         getId( 'FormSearch' ).click();
+//     }
+// });
--- skill/js/list.js
+++ skill/js/list.js
@@ -1,3 +1,185 @@
+var List_Array = [],
+    List_Total,
+    Search_Array = [],
+    Search_Total,
+    Page_All,
+    Page_Num,
+    Page_Len = 10;
+
+// Page --------------------------------------------------
+
+// func 產生頁碼
+const PageHtml = () => {
+
+    getId( 'PageList' ).innerHTML = '';
+
+    var l = [];
+    
+    // 頁碼dom
+    const p = ( n ) => {
+
+        var h = `<button class="page-li page-btn" id="${ 'Page' + n }" onclick="PageClick(${ n })">${ n }</button>`;
+
+        if( n === '' ) h = '<div class="page-dot">···<div>';
+
+        return `<li>${ h }</li>`;
+    };
+
+    // 不超過7頁
+    if ( Page_All <= 7 ) {
+
+        for( let i = 1 ; i <= Page_All ; i++ ) {
+            l.push( i )
+        }
+
+    // 超過7頁
+    } else {
+
+        var f = Page_Num - 2,
+            b = Page_Num + 2;
+
+        // 第一頁
+        if( f - 1 === 1 ) {
+            l.push( 1 )
+
+        } else if( f - 1 > 1 ) {
+            l.push( 1 , '' )
+        }
+
+        // 前後2頁
+        if( f - 1 < 1 ) {
+            f = 1;
+            b = f + 4;
+
+        } else if( b + 1 > Page_All ) {
+            b = Page_All;
+            f = b - 4;
+        }
+
+        for( let i = f ; i <= b ; i++ ){
+            l.push( i )
+        }
+        
+        // 最終頁
+        if( b + 1 === Page_All ) {
+            l.push( Page_All )
+
+        } else if( b + 1 < Page_All ) {
+            l.push( '' , Page_All )
+        }
+    }
+    // 產生dom
+    l.forEach( el => 
+        getId( 'PageList' ).insertAdjacentHTML( 'beforeend' , p( el ) ) 
+    );
+};
+
+// func 頁碼點選
+const PageClick = ( n ) => {
+    Page_Num = n;
+    PageHtml();
+    ListHtml();
+};
+
+// func 頁碼判斷
+const PageJudge = () => {
+
+    if ( Page_All <= 1 ) {
+        getId( 'PagePrev' ).disabled = true;
+        getId( 'PageNext' ).disabled = true;
+
+    } else if ( Page_Num === 1 ) {
+        getId( 'PagePrev' ).disabled = true;
+        getId( 'PageNext' ).disabled = false;
+
+    } else if ( Page_Num === Page_All ) {
+        getId( 'PagePrev' ).disabled = false;
+        getId( 'PageNext' ).disabled = true;
+        
+    } else {
+        getId( 'PagePrev' ).disabled = false;
+        getId( 'PageNext' ).disabled = false;
+    };
+
+    try {
+        queOne( '.page-li.--click' ).classList.remove( '--click' );
+    } catch {};
+
+    try {
+        getId( 'Page' + Page_Num ).classList.add( '--click' );
+    } catch {};
+};
+
+// event 上一頁
+getId( 'PagePrev' ).onclick = () => PageClick( Page_Num - 1 );
+
+// event 下一頁
+getId( 'PageNext' ).onclick = () => PageClick( Page_Num + 1 );
+
+
+// List --------------------------------------------------
+
+// event 執行搜尋
+getId( 'FormSearch' ).onclick = () => {
+
+    Loading( true );
+    ListFilter();
+
+    var s = queOne( '.td-sort-btn.--click' ),
+        v = s.value;
+
+    // 處理排序
+    Search_Array.sort( ( a , b ) => {
+
+        if( s.classList.contains( '--max' ) ) {
+            return a[ v ] < b[ v ] ? 1 : -1
+
+        } else if( s.classList.contains( '--min' ) ) {
+            return a[ v ] > b[ v ] ? 1 : -1
+        }
+    });
+    
+    // 處理參數
+    Search_Total = Search_Array.length;
+    Page_All     = Math.ceil( Search_Total / Page_Len );
+    Page_Num     = 1;
+    
+    setTimeout( () => {
+
+        Loading( false );
+        PageHtml();
+        ListHtml();
+
+        getId( 'SearchTotal' ).innerHTML = '共 ' + Search_Total + ' 筆資料';
+        
+    } , 300 );
+};
+
+// event 重設搜尋
+getId( 'FormReset' ).onclick = () => {
+
+    queAll( '.form-input' ).forEach( el => el.value = '' );
+    getId( 'FormSearch' ).click();
+};
+
+// event 切換排序
+queAll( '.td-sort-btn' ).forEach( el => {
+
+    el.onclick = () => {
+
+        if( el.classList.contains( '--click' ) ) {
+
+            el.classList.toggle( '--max' );
+            el.classList.toggle( '--min' );
+        }
+
+        queOne( '.td-sort-btn.--click' ).classList.remove( '--click' );
+        el.classList.add( '--click' );
+
+        getId( 'FormSearch' ).click();
+    }
+});
+
 // const
 const FormLink = ( id ) => { return `/admin/skill/upload.html?id=${ id }` };
 
